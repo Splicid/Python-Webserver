5a01e3d01711a1df84f274eda535ad13ae21dc71
$ ruby -v <luis1abreu11@gmail.com>
2021-06-14 23:05:19 -0400

    Added update function

--- app.py
+++ app.py
@@ -37,7 +37,22 @@ def data():
     else:
         friends = Friends.query.order_by(Friends.date_created)
         return render_template("data.html", title=title, friends=friends)
-    
+
+@app.route('/update/<int:id>', methods=['POST', 'GET'])
+def update(id):
+    friend_to_update = Friends.query.get_or_404(id)
+    if request.method == "POST":
+        friend_to_update.name = request.form['name']
+        try:
+            db.session.commit()
+            return redirect('/data')
+        except:
+            return "There was a problem updating the database"
+    else:
+        return render_template('update.html',friend_to_update=friend_to_update)
+
+
+
 @app.route('/')
 def index():
     title = "Database Book"
